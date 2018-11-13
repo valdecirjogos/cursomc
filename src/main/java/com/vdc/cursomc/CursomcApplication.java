571a4e42bc3dbae08ab0926348f3b042e4117e6e
valdecir <valdecir.neper@gmail.com>
2018-11-13 14:32:48 -0200

    Produtos e associacao m to m

--- src/main/java/com/vdc/cursomc/CursomcApplication.java
+++ src/main/java/com/vdc/cursomc/CursomcApplication.java
@@ -8,12 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.vdc.cursomc.domain.Categoria;
+import com.vdc.cursomc.domain.Produto;
 import com.vdc.cursomc.repositories.CategoriaRepository;
+import com.vdc.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);  
@@ -25,6 +31,20 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null,"informática");
 		Categoria cat2 = new Categoria(null, "escritório");
 		
+		Produto p1 = new Produto(null,"computador",2000.00);
+		Produto p2 = new Produto(null,"impressora",800.00);
+		Produto p3 = new Produto(null,"mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat1.getProdutos().addAll(Arrays.asList(p2)); 
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+	
 	}
 }
---
+++ src/main/java/com/vdc/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.vdc.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vdc.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
+	
+}
